a583bef3149d18d201f8b0398d22c7132b00ce3f
Sandeepm1193 <103820080+Sandeepm1193@users.noreply.github.com>
2024-11-10 19:23:12 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -1,13 +1,14 @@
 provider "aws"{
-region = "eu-west-2"
+    region="eu-north-1"
 }
 
-resource "aws_instance" "AWSINSTANCE"{
-ami = "ami-0acc77abdfc7ed5a6"
-instance_type = "t2.micro"
-key_name = "devopsaug2024"
-security_groups = ["launch-wizard-2"]
+resource "aws_instance" "AWSEC2Instance"{
+ami = "ami-04b54ebf295fe01d7"
+instance_type = "t3.micro"
+key_name = "gitRSA"
+security_groups =["launch-wizard-2"]
 tags={
-Name = "terraformserver"
+    name = "terraformserver"
 }
+
 }
